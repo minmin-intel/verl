d06940eb6a29fe3db6e3f1b48f7d2915611d3d3d
minmin-intel <minmin.hou@intel.com>
2025-07-30 16:59:11 -0700

    first code to support xpu

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -58,6 +58,7 @@ def run_ppo(config) -> None:
         ray.init(
             runtime_env=get_ppo_ray_runtime_env(),
             num_cpus=config.ray_init.num_cpus,
+            num_gpus=config.ray_init.num_gpus,
         )
 
     # Create a remote instance of the TaskRunner class, and
